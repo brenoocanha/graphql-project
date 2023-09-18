71d2e9ee12abaef2a4062831e879c3b404907ba6
Breno Ocanha <breno@dmxdesign.com.br>
2023-09-18 17:32:59 -0300

    feat: apollo server

--- server/server.js
+++ server/server.js
@@ -1,3 +1,6 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from '@apollo/server/standalone'
+
 const typeDefs = `#graphql
     type Query {
         greeting: String
@@ -8,4 +11,8 @@ const resolvers = {
     Query: {
         greeting: () => 'Helo world!',
     }
-}
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+const { url } = await startStandaloneServer(server, { listen: { port: 9000 } });
+console.log(`Server running at ${url}`);
